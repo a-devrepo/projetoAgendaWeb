fdd11cb23f3b44185b736fa8d01ecc067ab640ac
alison <adevpr11@gmail.com>
2025-06-03 13:05:31 -0300

    Exibe lista de categorias no formulário de cadastro

--- src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,20 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-tarefas',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './cadastro-tarefas.component.html',
   styleUrl: './cadastro-tarefas.component.css'
 })
 export class CadastroTarefasComponent {
   
+  categorias: any[] = [];
+
   http = inject(HttpClient);
   
   ngOnInit(): void {
     this.http.get('http://localhost:8081/api/v1/categorias')
     .subscribe((response) =>{
-      console.table(response);
+      this.categorias = response as any[];
     });
   }
 }
