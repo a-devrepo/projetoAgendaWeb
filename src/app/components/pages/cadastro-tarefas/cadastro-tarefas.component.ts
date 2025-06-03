f2039de486f7daa3334d86571d7153309129ad5e
alison <adevpr11@gmail.com>
2025-06-03 12:34:15 -0300

    Consulta ao endpoint de categorias

--- src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro-tarefas',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './cadastro-tarefas.component.css'
 })
 export class CadastroTarefasComponent {
-
+  
+  http = inject(HttpClient);
+  
+  ngOnInit(): void {
+    this.http.get('http://localhost:8081/api/v1/categorias')
+    .subscribe((response) =>{
+      console.table(response);
+    });
+  }
 }
