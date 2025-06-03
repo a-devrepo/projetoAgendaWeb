d8803e389f44e5c31e97f26b94ca05dab110278d
alison <adevpr11@gmail.com>
2025-06-03 11:04:33 -0300

    Remove lógica de consulta ao backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,12 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  private http = inject(HttpClient);
-
-  ngOnInit(): void {
-    this.http.get('http://localhost:8081/api/v1/categorias')
-    .subscribe((dados) =>{
-      console.log(dados);
-    })
-}
+  
 }
