c10f5e1b59de18e6a9a7319820398cf60c5faa0a
alison <adevpr11@gmail.com>
2025-06-05 10:00:40 -0300

    Adiciona formulário reativo ao cadastro de tarefas

--- src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-tarefas',
@@ -15,7 +16,16 @@ export class CadastroTarefasComponent {
   categorias: any[] = [];
 
   http = inject(HttpClient);
+  formBuilder = inject(FormBuilder);
   
+  form = this.formBuilder.group({
+    titulo: new FormControl('',[Validators.required]),
+    data: new FormControl('',[Validators.required]),
+    hora: new FormControl('',[Validators.required]),
+    fnalizado: new FormControl('',[Validators.required]),
+    categoriaId: new FormControl('',[Validators.required])
+  });
+
   ngOnInit(): void {
     this.http.get('http://localhost:8081/api/v1/categorias')
     .subscribe((response) =>{
